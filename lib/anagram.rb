aa01e37d182b83f6c1ac065ee75683f39dfaefd3
github username <github email address>
2018-12-14 17:41:51 +0000

    learn submit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+class Anagram
+  def initialize (word)
+    @word = word
+  end
+
+  def is_anagram?(word)
+    included = true
+    word_arr = @word.split("")
+    word_arr.each do |letter|
+
+      if !(word.include? (letter) )
+        included = false
+      end
+    end
+    if word.length == @word.length
+      included
+    else
+      false
+    end
+  end
+
+  def match(ana_array)
+
+    ana_array.select do |x|
+     is_anagram?(x)
+    end
+  end
+
+end
